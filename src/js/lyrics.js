f9c84a9ade4a6736725346a94b0ae1c7d5a6e163
Attila Gyomber <gyomberattila@gmail.com>
2020-03-02 13:52:24 +0100

    lyrics div fade out

--- src/js/lyrics.js
+++ src/js/lyrics.js
@@ -2,14 +2,16 @@ const lyricsBtn = document.getElementById('lyrics_btn');
 
 lyricsBtn.addEventListener('click', () => {
     const lyricsDiv = document.getElementById('lyrics');
-    if(lyricsDiv.style.display == '') {
-        lyricsDiv.style.display = 'none';
+    if (!lyricsDiv.classList.contains('lyrics_hidden')) {
+        lyricsDiv.classList.add('lyrics_hidden');
     } else {
-        lyricsDiv.style.display == 'block'
+        lyricsDiv.classList.remove('lyrics_hidden');
+        lyricsDiv.classList.add('lyrics_visible');
     }
 
     /* changes lyrics button icon */
     const lyricsBtnIcon = document.getElementById('lyrics_btn_icon');
+
     if (lyricsBtnIcon.src == 'file://' + __dirname + '/src/assets/icons/back.png') {
         lyricsBtnIcon.src = './src/assets/icons/next.png';
     } else {
